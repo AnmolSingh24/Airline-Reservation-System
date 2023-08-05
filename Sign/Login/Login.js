2276bf36b77b095919b59cef396bd8ec869a5666
Anmol <as0796348@gmail.com>
2023-08-05 21:20:50 +0530

    User found changes done

--- Sign/Login/Login.js
+++ Sign/Login/Login.js
@@ -1,11 +1,18 @@
-var users = JSON.parse(localStorage.getItem("userData")) || [];
-console.log(users);
-
 const login = (e) => {
     e.preventDefault();
+    var users = JSON.parse(localStorage.getItem("userData")) || [];
+    console.log(users);
     const loginUsername = document.getElementById("username");
     const loginPassword = document.getElementById("password");
-    const userFound = users?.find(user => user.username === loginUsername.value.trim() && user.password === loginPassword.value.trim());
+    let userFound;
+    let size = users.length;
+    for (let i = 0; i < size; i++) {
+        let user = users[i];
+        if (user.username === loginUsername.value.trim() && user.password === loginPassword.value.trim()) {
+            userFound = user; break;
+        }
+    }
+    console.log(userFound);
     if (userFound) {
 
         var msg = document.getElementById("snackbar");
@@ -13,11 +20,19 @@ const login = (e) => {
         setTimeout(() => {
             msg.className = msg.className.replace("show", "");
         }, 4000);
-
-        window.location = "index.html"
-    } else {
-        alert("❌ Incorrect username or password");
+        return;
     }
+    if (loginUsername !== userFound && loginPassword !== userFound) {
+        var errorMsg = document.getElementById("errorSnackbar");
+        errorMsg.className = "showError";
+        setTimeout(() => {
+            msg.className = errorMsg.className.replace("showError", "");
+        }, 4000);
+        return;
+    }
+
+    alert("❌ Incorrect username or password");
+
 }
 
 document.getElementById("login").addEventListener('click', login);
