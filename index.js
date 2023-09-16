3b36b35df93c437106328094cc9199d66cdcdb74
Anmol <as0796348@gmail.com>
2023-09-16 09:48:42 +0530

    check button added

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ $(document).ready(function () {
         interval: 1500
     })
 });
+
+// Select ways (onw way or round trip)
+const radioButtons = document.getElementsByName("way");
+
+for (const radioButton of radioButtons) {
+    if (radioButton.checked) {
+        console.log("Selected way: " + radioButton.value);
+        break; // Exit the loop since only one can be checked
+    }
+}
