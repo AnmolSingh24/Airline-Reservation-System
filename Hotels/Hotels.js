81970679a66e4ecb8c97e72f209b9e7a15b7fde4
Anmol <as0796348@gmail.com>
2023-10-02 21:32:48 +0530

    json removed

--- Hotels/Hotels.js
+++ Hotels/Hotels.js
@@ -1,39 +1,4 @@
-const hotelData = [{
-    img: `/images/Hotel1.jpg`,
-    title: "Royal Orchid Sheraton Hotel & Towers",
-    location: "Bankok, Thailand",
-    price: "$60 per night"
-},
-{
-    img: `/images/Hotel2.jpg`,
-    title: "The Taj Palace",
-    location: "New Delhi, India",
-    price: "$100 per night"
-},
-{
-    img: `/images/Hotel3.jpg`,
-    title: "Lara Beach Hotel",
-    location: "Antalya, Turkey",
-    price: "$150 per night"
-},
-{
-    img: `/images/Hotel4.webp`,
-    title: "Disney's Contemporary Resort",
-    location: "Florida, United States",
-    price: "$120 per night"
-},
-{
-    img: `/images/Hotel5.jpg`,
-    title: "The Taj Hotel",
-    location: "Mumbai, India",
-    price: "$200 per night"
-},
-{
-    img: `/images/Hotel6.jpg`,
-    title: "The Claridge Hotel",
-    location: "New Delhi, India",
-    price: "$250 per night"
-}];
+const hotelData = [];
 
 const hotelItems = document.querySelectorAll('.hotel-items');
 const showHotel = () => {
@@ -57,3 +22,99 @@ const showHotel = () => {
     });
 }
 showHotel();
+
+async function hotelSearch1() {
+    const url = 'https://hotels-com-provider.p.rapidapi.com/v2/hotels/search?sort_order=REVIEW&locale=en_GB&checkin_date=2023-09-26&adults_number=1&domain=AE&region_id=2872&checkout_date=2023-09-27&payment_type=PAY_LATER%2CFREE_CANCELLATION&price_min=10&guest_rating_min=8&meal_plan=FREE_BREAKFAST&page_number=1&available_filter=SHOW_AVAILABLE_ONLY&children_ages=4%2C0%2C15&star_rating_ids=3%2C4%2C5&price_max=500&amenities=WIFI%2CPARKING&lodging_type=HOTEL%2CHOSTEL%2CAPART_HOTEL';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '59f66912aemshc9f0db0d32e7ebfp15542ajsna3f41fe29ff6',
+            'X-RapidAPI-Host': 'hotels-com-provider.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        return result.properties;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+const showResult = async () => {
+    const result = await hotelSearch1();
+    console.log(result);
+    let index = "";
+    for (const hotel of result) {
+        const {
+            name,
+            price: { lead: { formatted } },
+            propertyImage: { image: { url } },
+            star
+        } = hotel;
+        console.log({ name, formatted, url, star });
+
+        // Loop through the result array
+        for (let i = 0; i < result.length; i++) {
+            // Generate HTML for each object in the array
+            let innerHTML = `
+              <div class="hotel-card">
+                <img src="${result[i].propertyImage.image.url}">
+                <div class="hotel-details">
+                  <h3>${result[i].name}</h3>
+                  <p><b>Price: </b>${result[i].price.lead.formatted} per night</p>
+                  <p><b>Ratings: </b>${result[i].star}/5</p>
+                  <a href="/">View Info</a>
+                </div>
+              </div>`;
+
+            index += innerHTML; // Append the HTML to the string
+        }
+    };
+    hotelItems.forEach((e) => {
+        e.innerHTML = index;
+    });
+}
+showResult();
+
+
+// async function hotelSearch2() {
+//     const url = 'https://airbnb-search.p.rapidapi.com/property/search?query=New%20York%2C%20NY';
+//     const options = {
+//         method: 'GET',
+//         headers: {
+//             'X-RapidAPI-Key': '59f66912aemshc9f0db0d32e7ebfp15542ajsna3f41fe29ff6',
+//             'X-RapidAPI-Host': 'airbnb-search.p.rapidapi.com'
+//         }
+//     };
+
+//     try {
+//         const response = await fetch(url, options);
+//         const result = await response.json();
+//         console.log(result);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+// async function hotelSearch3() {
+//     const url = 'https://countriesnow.space/api/v0.1/countries';
+//     const options = {
+//         method: 'GET',
+//         // headers: {
+//         //     'X-RapidAPI-Key': '59f66912aemshc9f0db0d32e7ebfp15542ajsna3f41fe29ff6',
+//         //     'X-RapidAPI-Host': 'hotels-com-provider.p.rapidapi.com'
+//         // }
+//     };
+
+//     try {
+//         const response = await fetch(url, options);
+//         const result = await response.json();
+//         console.log(result);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+// hotelSearch3();
